0110a469c67994a829b2e179a6756f89c56fa03f
VLx2000 <victor.antunes.2000@gmail.com>
2022-09-25 14:49:10 -0300

    adicionando verificações

--- frontend/src/pages/Clientes/criar.jsx
+++ frontend/src/pages/Clientes/criar.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { Alert, Button, Container, Form } from "react-bootstrap";
+//import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axios";
 import { Voltar } from "../../components";
+import { verificaCPF, verificaNome, verificaObservacao } from "../../utils/verificacoes";
 
 // component q exibe form para criar novo cliente
 function NovoCliente() {
 
-    const navigate = useNavigate();
+    //const navigate = useNavigate();
 
     const [nome, setNome] = useState('');
     const [nascimento, setNascimento] = useState('');
@@ -16,6 +17,12 @@ function NovoCliente() {
     const [email, setEmail] = useState('');
     const [endereco, setEndereco] = useState('');
     const [observacao, setObservacao] = useState('');
+
+    const [erroNome, setErroNome] = useState(false);
+    const [erroCpf, setErroCpf] = useState(false);
+    const [erroObs, setErroObs] = useState(false);
+
+    const [msg, setMsg] = useState('');
 
     const submitHandler = async (e) => {
         e.preventDefault();
@@ -26,24 +33,47 @@ function NovoCliente() {
                 //{ headers: { "Content-Type": 'application/json', } }
             )
             .then((res) => {
-                navigate('/');
+                //navigate('/');
+                console.log(res.data.message)
+                setMsg(res.data.message);
             })
             .catch((error) => {
-                console.log('erro')
+                setMsg(error.data.message);
             });
     };
+
+    function handleNome(nome) {
+        setErroNome(verificaNome(nome));
+        setNome(nome);
+    }
+
+    function handleCPF(cpf) {
+        setErroCpf(verificaCPF(cpf));
+        setCpf(cpf);
+    }
+
+    function handleObs(obs) {
+        setErroObs(verificaObservacao(obs));
+        setObservacao(obs);
+    }
 
     return (
         <Container>
             <Voltar caminho={'/'} />
             <Container className="login-container">
                 <Form onSubmit={submitHandler}>
+                    {msg && <Alert variant="warning">{msg}</Alert>}
                     <h3 className="titulo-pag">Adicionar novo cliente</h3>
                     <Form.Group controlId="nome" className="mb-3">
                         <Form.Label column sm="6">Nome</Form.Label>
                         <Form.Control
-                            onChange={(e) => setNome(e.target.value)}
+                            required
+                            isInvalid={erroNome}
+                            onChange={(e) => handleNome(e.target.value)}
                         />
+                        <Form.Control.Feedback type="invalid">
+                            Nome não aceita símbolos
+                        </Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group controlId="nasc" className="mb-3">
                         <Form.Label column sm="6">Data de nascimento</Form.Label>
@@ -57,8 +87,12 @@ function NovoCliente() {
                         <Form.Label column sm="6">CPF</Form.Label>
                         <Form.Control
                             required
-                            onChange={(e) => setCpf(e.target.value)}
+                            isInvalid={erroCpf}
+                            onChange={(e) => handleCPF(e.target.value)}
                         />
+                        <Form.Control.Feedback type="invalid">
+                            CPF inválido
+                        </Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group controlId="celular" className="mb-3">
                         <Form.Label column sm="6">Celular</Form.Label>
@@ -85,12 +119,16 @@ function NovoCliente() {
                     <Form.Group controlId="obs" className="mb-3">
                         <Form.Label column sm="6">Observação</Form.Label>
                         <Form.Control
-                            type='textArea'
-                            onChange={(e) => setObservacao(e.target.value)}
+                            isInvalid={erroObs}
+                            as="textarea" rows={3}
+                            onChange={(e) => handleObs(e.target.value)}
                         />
+                        <Form.Control.Feedback type="invalid">
+                            O limite é de 300 caracteres
+                        </Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group controlId="criar" className="mb-3 div-botao-entrar">
-                        <Button type="submit">Criar cliente</Button>
+                        <Button type="submit" disabled={erroCpf || erroNome || erroObs}>Criar cliente</Button>
                     </Form.Group>
                 </Form>
             </Container>
--- frontend/src/pages/Clientes/editar.jsx
+++ frontend/src/pages/Clientes/editar.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import { Alert, Button, Container, Form, Spinner } from "react-bootstrap";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axiosInstance from "../../utils/axios";
 import { Voltar } from "../../components";
+import { verificaCPF, verificaNome, verificaObservacao } from "../../utils/verificacoes";
 
 // component q exibe form para editar cliente
 function EditarCliente() {
 
     const params = useParams();
-    const navigate = useNavigate();
+    //const navigate = useNavigate();
 
     const [erro, setErro] = useState('');
     const [carregado, setCarregado] = useState(false);
@@ -21,6 +22,12 @@ function EditarCliente() {
     const [email, setEmail] = useState('');
     const [endereco, setEndereco] = useState('');
     const [observacao, setObservacao] = useState('');
+
+    const [erroNome, setErroNome] = useState(false);
+    const [erroCpf, setErroCpf] = useState(false);
+    const [erroObs, setErroObs] = useState(false);
+
+    const [msg, setMsg] = useState('');
 
     useEffect(() => {
         axiosInstance
@@ -62,10 +69,12 @@ function EditarCliente() {
                 { headers: { "Content-Type": 'application/json', } }
             )
             .then((res) => {
-                navigate('/');
+                //navigate('/');
+                console.log(res.data.message)
+                setMsg(res.data.message);
             })
             .catch((error) => {
-                console.log('epa')
+                setMsg(error.data.message);
             });
     };
 
@@ -73,12 +82,29 @@ function EditarCliente() {
         axiosInstance
             .delete('/delete.php/?id=' + params.idCliente)
             .then((res) => {
-                navigate('/');
+                //navigate('/');
+                console.log(res.data.message)
+                setMsg(res.data.message);
             })
             .catch((error) => {
-                console.log('epa')
+                setMsg(error.data.message);
             });
     };
+
+    function handleNome(nome) {
+        setErroNome(verificaNome(nome));
+        setNome(nome);
+    }
+
+    function handleCPF(cpf) {
+        setErroCpf(verificaCPF(cpf));
+        setCpf(cpf);
+    }
+
+    function handleObs(obs) {
+        setErroObs(verificaObservacao(obs));
+        setObservacao(obs);
+    }
 
     return (
         <Container>
@@ -87,14 +113,19 @@ function EditarCliente() {
             {carregado ?
                 <Container className="login-container">
                     <Form onSubmit={submitHandler}>
+                        {msg && <Alert variant="warning">{msg}</Alert>}
                         <h3 className="titulo-pag">Editar cliente {cliente?.id}</h3>
                         <Form.Group controlId="nome" className="mb-3">
                             <Form.Label column sm="6">Nome</Form.Label>
                             <Form.Control
+                                isInvalid={erroNome}
                                 required
                                 defaultValue={nome}
-                                onChange={(e) => setNome(e.target.value)}
+                                onChange={(e) => handleNome(e.target.value)}
                             />
+                            <Form.Control.Feedback type="invalid">
+                                Nome não aceita símbolos
+                            </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group controlId="nasc" className="mb-3">
                             <Form.Label column sm="6">Data de nascimento</Form.Label>
@@ -109,9 +140,13 @@ function EditarCliente() {
                             <Form.Label column sm="6">CPF</Form.Label>
                             <Form.Control
                                 required
+                                isInvalid={erroCpf}
                                 defaultValue={cpf}
-                                onChange={(e) => setCpf(e.target.value)}
+                                onChange={(e) => handleCPF(e.target.value)}
                             />
+                            <Form.Control.Feedback type="invalid">
+                                CPF inválido
+                            </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group controlId="celular" className="mb-3">
                             <Form.Label column sm="6">Celular</Form.Label>
@@ -142,12 +177,16 @@ function EditarCliente() {
                             <Form.Label column sm="6">Observação</Form.Label>
                             <Form.Control
                                 defaultValue={observacao}
-                                type='textArea'
-                                onChange={(e) => setObservacao(e.target.value)}
+                                isInvalid={erroObs}
+                                as="textarea" rows={3}
+                                onChange={(e) => handleObs(e.target.value)}
                             />
+                            <Form.Control.Feedback type="invalid">
+                                O limite é de 300 caracteres
+                            </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group controlId="criar" className="mb-3 div-botao-entrar">
-                            <Button variant="secondary" type="submit">Salvar</Button>
+                            <Button variant="secondary" disabled={erroCpf || erroNome || erroObs} type="submit">Salvar</Button>
                             <Button variant="outline-danger" onClick={deletar}>Apagar cliente</Button>
                         </Form.Group>
                     </Form>
